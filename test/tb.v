82727b6e08a05d0c61e9393a50597b9c5e9812f6
Leon Pehmer <leon.pehmer@student.tugraz.at>
2025-10-08 10:48:09 +0200

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_phemi6_lfsr dut (
+  tt_um_phemi6_lfsr user_project  (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
